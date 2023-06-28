374cc4aee74f3ebf759057cfc534e8468a8fbd73
DEV <dev@step.rivne>
2023-06-28 20:51:33 +0300

    string

---
+++ 05_cycles_help_home.py
@@ -0,0 +1,60 @@
+
+
+# print(" "*0, "*"*10, sep="")
+# print(" "*1, "*"*9, sep="")
+# print(" "*2, "*"*8, sep="")
+# print(" "*3, "*"*7, sep="")
+# print(" "*4, "*"*6, sep="")
+# print(" "*5, "*"*5, sep="")
+
+# star = int(input("Enter size"))
+# for i in range(star):  # 0,1,2,3,....9
+#     print(" "*i, "*"*star, sep="")
+#     star -= 1
+
+
+# *****
+#  ***
+#   *
+#   *
+#   ***
+#  *****
+
+# print(" "*0, "*" * 5, sep="")
+# print(" "*1, "*" * 3, sep="")
+# print(" "*2, "*" * 1, sep="")
+
+# space = 0
+# star = 11
+# number = star
+# i = 0
+# flag = True
+# while i < number:
+#     i += 1
+#     print(' '*space, "*"*star, sep="")
+#     if star == 1:
+#         flag = False
+#     if flag:
+#         star -= 2
+#         space += 1
+#     else:
+#         star += 2
+#         space -= 1
+
+
+# space = 8
+# star = 1
+# number = 9
+# i = 0
+# flag = True
+# while i < number:
+#     i += 1
+#     print('*'*star, ' '*space, "*"*star, sep="")
+#     if star == 5:
+#         flag = False
+#     if flag:
+#         star += 1
+#         space -= 2
+#     else:
+#         star -= 1
+#         space += 2
---
+++ 07_string.py
@@ -0,0 +1,369 @@
+
+
+import re
+line = 'Lorem ipsum dolor'
+line_2 = "I \n\"love\"\t Python"
+line_3 = "\\n - enter"
+raw = r"\n - enter"
+img = '''
+                                                                                                    
+       ^!!~^^:.                                                                     ..:^~!!7.       
+       &@@@@@@@@&&GY!:                                                       .^?PB&@@@@@@@@@:       
+       G@@PYPG#&@@@@@@@#Y^                                               :7G&@@@@@@@&#G5J&@@.       
+       J@@~     .:?G&@@@@@@B7.                                        ^5&@@@@@@#Y~.      #@&        
+       ^@@G          ^5@@@@@@@G^      ..                   ..      .Y&@@@@@@G~          .@@5        
+        @@@.            ?@@@@@@@&7    Y&@G^             .Y#@&:   ^B@@@@@@@P.            Y@@:        
+        G@@Y             .B@@@@@@@&7    :JBG^         :YP?^.   ^#@@@@#@@@^             .@@&         
+        ~@@@.              B@7&@@@@@#:      ~!       ~~      .G@@@@@@^&@.              P@@7         
+         &@@B              .@^.&@@@@@@5       :     :       7@@@@@@@! &7              ~@@@          
+         7@@@Y              #G  JJ7&P#@&.      .   .       G@@5#5!?. ~@.             :@@@5          
+          &@@@Y             &@5     . ?@@~      .  .     .&@#. .    ~@@?            ^@@@@.          
+          ^@@@@G.          B@@@&J.     ^!B7     . ^     :&77      !#@@@@!          J@@@@J           
+           5@@@@@5:     :J@@@@@@@@&Y:     &?    .:!    :@!    :?B@@@@@@@@#!.    :J&@@@@#            
+            #@@@@@@@&&&@@@@@@@#BPYPG#B5~  :B7    ^7   .B?  ^5##BPY5PB&@@@@@@@&&@@@@@@@@.            
+             &@Y5#@@@@@@@@@#!.        :?P5. #~  :#Y   #: JB5^         :5@@@@@@@@@&BY&@~             
+             .&@7  .^~!^7@Y              :GY!@. G@#  PG^#?              .&G^!!~:. .#@!              
+              .&@P      &G      ^#^.!YP5?. ~B@B J@@.^@&5 .!5GPJ:.#J      .@!     :@@~               
+                P@P    7@.     7@@#@@&#&@@&!^@@7&@@J#@J:B@@@@@@@&@@#      G@    :@&:                
+                 !@G   5@.  .P.@@@@7     ~B@#?@@@@@@@PY@&?:.  .?@@@@?7J   G@.  ^@P                  
+                  .G#: !@?  .@@@@@J        :&&&@@@@@@#@!        !@@@@@5  .@&  Y#^                   
+                    :55~&@7  Y@@@@#:  .^77~. P@@@@@@@&. .^^:.   J@@@@@.  #@JYG!                     
+                      .?#@@#^ !&@@@@@#BG5?7?7:G@@@@@@?JY!!?Y5P#&@@@@5. 7@@&5^                       
+                         .7B&&P5G&@@@@&57~~~!JB@@@@@@G7~~!7YG&@@@@#5YG##Y^                          
+                             .^!?YG#@@@&B5?JG&@@@@@@@@GY7JP#@@@&BY?!^.                              
+                               :?G&@B!.   !J7@@G@@@&@@Y^?.   :J&@BJ:                                
+                            :5&@@@&^    :B! 5@#:@@@J5@@ .G?     B@@@@P:                             
+                          .G@@@@@&.    ?@7  &@: @@@! &@^  @B     G@@@@@B:                           
+                         ~@@@@@@@.    B&&   @J  &@@: ^@?  G##^    B@@@@@@?                          
+                        !@@@@@@@:   7@5:@  !B   J@&   JB  #J.@P.   #@@@@@@5                         
+                       :@@@@@@#. :Y&@P .@. G    .@P    P~ &? ^@@B!  G@@@@@@J                        
+                       &@@@@@G?P&@@@&  .@~Y.     ?^     5^#~  5@@@@BYG@@@@@@^                       
+                      !@@@@@@@@@@@@@Y  .@B.              Y@7  :@@@@@@@@@@@@@#                       
+                      #@@@@G!?G@@@@@^  Y&.                Y&   @@@@@#Y!?&@@@@:                      
+                     :@@@@^    .#@@@  7G                   !P  #@@@~     P@@@?                      
+                     7@@@^      .@@Y.5?                     :P!7@@5       #@@B                      
+                     5@@G       !@@##^                        Y&@@#       ~@@&                      
+                     5@@!      ?@@@J                           .G@@B.      @@&                      
+                     7@@:   .7&@@5.                              :B@@P:    #@B                      
+                     ^@@:.?#@@&J.                                  .5@@&5~ B@5                      
+                     .@@@@@@G^                                        ~G@@@@@!                      
+                      B@BJ:                                              :JB@:                      
+                                                                                                    
+'''
+# print(line)
+# print(line_2)
+# print(line_3)
+# print(raw)
+# print(img)
+
+# name text ... name text .... age name text.......
+# name = 'Nazar'
+# age = 22
+# formatLine = "{0} text {1} .... {0} text .... {1} {0} text ..... ".format(name, age)
+
+# print(formatLine, type(formatLine))
+
+
+# line = "Lorem ipsum dolor"
+# print(id(line), line)
+# line += " !!!!!!"
+# print(id(line), line)
+
+# for i in range(1000000):
+#     print(i, ' :: ', chr(i), end="\t")
+#     if i % 7 == 0:
+#         print()
+
+
+# print(ord('A'))
+# print(ord('Z'))
+
+
+# print(line)
+# print(line[0])
+# print(len(line))
+# print(len(line)-1, line[len(line)-1])
+
+# print(line[2:-3])
+# print(line[2::4])
+
+
+numbers = '12345874'
+letters = 'jghujdmffbvg'
+line = 'Lorem ipsum emet dolor'
+word = 'Fruit Apple'
+word_2 = "BANANA"
+letterDig = 'hfgt3546jfhfy'
+
+# лише букви або цифри
+print('\n\n================== isalnum() =================')
+print('\t', numbers, '\t--- >\t', numbers.isalnum())
+print('\t', letters, '\t--- >\t', letters.isalnum())
+print(line, ' --- >\t', line.isalnum())
+print('\t', word, '\t--- >\t', word.isalnum())
+print('\t', word_2, '\t--- >\t', word_2.isalnum())
+print('\t', letterDig, '\t--- >\t', letterDig.isalnum())
+print('==================================================')
+
+print('\n\n================== isalpha() =================')  # лише букви
+print('\t', numbers, '\t--- >\t', numbers.isalpha())
+print('\t', letters, '\t--- >\t', letters.isalpha())
+print(line, ' --- >\t', line.isalpha())
+print('\t', word, '\t--- >\t', word.isalpha())
+print('\t', word_2, '\t--- >\t', word_2.isalpha())
+print('\t', letterDig, '\t--- >\t', letterDig.isalpha())
+print('==================================================')
+
+print('\n\n================== isdigit() =================')  # лише цифри
+print('\t', numbers, '\t--- >\t', numbers.isdigit())
+print('\t', letters, '\t--- >\t', letters.isdigit())
+print(line, ' --- >\t', line.isdigit())
+print('\t', word, '\t--- >\t', word.isdigit())
+print('\t', word_2, '\t--- >\t', word_2.isdigit())
+print('\t', letterDig, '\t--- >\t', letterDig.isdigit())
+print('==================================================')
+
+# letters = '           '
+print('\n\n================== isspace() =================')  # лише пропуск
+print('\t', numbers, '\t--- >\t', numbers.isspace())
+print('\t', letters, '\t--- >\t', letters.isspace())
+print(line, ' --- >\t', line.isspace())
+print('\t', word, '\t--- >\t', word.isspace())
+print('\t', word_2, '\t--- >\t', word_2.isspace())
+print('\t', letterDig, '\t--- >\t', letterDig.isspace())
+print('==================================================')
+
+
+# перевірка букви на нижній регістр
+print('\n\n================== islower() =================')
+print('\t', numbers, '\t--- >\t', numbers.islower())
+print('\t', letters, '\t--- >\t', letters.islower())
+print(line, ' --- >\t', line.islower())
+print('\t', word, '\t--- >\t', word.islower())
+print('\t', word_2, '\t--- >\t', word_2.islower())
+print('\t', letterDig, '\t--- >\t', letterDig.islower())
+print('==================================================')
+
+
+# перевірка букви на верхній регістр
+print('\n\n================== isupper() =================')
+print('\t', numbers, '\t--- >\t', numbers.isupper())
+print('\t', letters, '\t--- >\t', letters.isupper())
+print(line, ' --- >\t', line.isupper())
+print('\t', word, '\t--- >\t', word.isupper())
+print('\t', word_2, '\t--- >\t', word_2.isupper())
+print('\t', letterDig, '\t--- >\t', letterDig.isupper())
+print('==================================================')
+
+
+#  першої букви у слові на верхній регістр
+print('\n\n================== istitle() =================')
+print('\t', numbers, '\t--- >\t', numbers.istitle())
+print('\t', letters, '\t--- >\t', letters.istitle())
+print(line, ' --- >\t', line.istitle())
+print('\t', word, '\t--- >\t', word.istitle())
+print('\t', word_2, '\t--- >\t', word_2.istitle())
+print('\t', letterDig, '\t--- >\t', letterDig.istitle())
+print('==================================================')
+
+
+print('\n\n================== lower() =================')
+print('\t', numbers, '\t--- >\t', numbers.lower())
+print('\t', letters, '\t--- >\t', letters.lower())
+print(line, ' --- >\t', line.lower())
+print('\t', word, '\t--- >\t', word.lower())
+print('\t', word_2, '\t--- >\t', word_2.lower())
+print('\t', letterDig, '\t--- >\t', letterDig.lower())
+print('==================================================')
+
+
+print('\n\n================== upper() =================')
+print('\t', numbers, '\t--- >\t', numbers.upper())
+print('\t', letters, '\t--- >\t', letters.upper())
+print(line, ' --- >\t', line.upper())
+print('\t', word, '\t--- >\t', word.upper())
+print('\t', word_2, '\t--- >\t', word_2.upper())
+print('\t', letterDig, '\t--- >\t', letterDig.upper())
+print('==================================================')
+
+
+print('\n\n================== capitalize() =================')
+print('\t', numbers, '\t--- >\t', numbers.capitalize())
+print('\t', letters, '\t--- >\t', letters.capitalize())
+print(line, ' --- >\t', line.capitalize())
+print('\t', word, '\t--- >\t', word.capitalize())
+print('\t', word_2, '\t--- >\t', word_2.capitalize())
+print('\t', letterDig, '\t--- >\t', letterDig.capitalize())
+print('==================================================')
+
+
+print('\n\n================== title() =================')
+print('\t', numbers, '\t--- >\t', numbers.title())
+print('\t', letters, '\t--- >\t', letters.title())
+print(line, ' --- >\t', line.title())
+print('\t', word, '\t--- >\t', word.title())
+print('\t', word_2, '\t--- >\t', word_2.title())
+print('\t', letterDig, '\t--- >\t', letterDig.title())
+print('==================================================')
+
+
+print('\n\n================== swapcase() =================')
+print('\t', numbers, '\t--- >\t', numbers.swapcase())
+print('\t', letters, '\t--- >\t', letters.swapcase())
+print(line, ' --- >\t', line.swapcase())
+print('\t', word, '\t--- >\t', word.swapcase())
+print('\t', word_2, '\t--- >\t', word_2.swapcase())
+print('\t', letterDig, '\t--- >\t', letterDig.swapcase())
+print('==================================================')
+
+line += ' ipsum lorem dolor ipsum'
+
+print('\n\n================== find(substr,startindex=default,endindex=default) =================\n')
+
+print('\t ', line, ' --- >\t', line.find('ipsum'))
+print('\t ', line, ' --- >\t', line.find('ipsum', 7))
+print('\t ', line, ' --- >\t', line.find('ipsum', 24))
+print('\t ', line, ' --- >\t', line.find('ipsum', 42))
+
+print('\n\n================== find() =================\n')
+index = -1
+while True:
+    index = line.find('ipsum', index+1)
+    if index == -1:
+        break
+    print('\t ', line, ' --- >\t', index)
+
+print('\n==================================================')
+
+print('\n\n================== find() =================\n')
+index = -1
+while True:
+    index = line.lower().find('lorem', index+1)
+    if index == -1:
+        break
+    print('\t ', line, ' --- >\t', index)
+
+print('\n==================================================')
+
+
+print('\n\n================== rfind(substr,startindex=default,endindex=default) =================\n')
+
+print('\t ', line, ' --- >\t', line.rfind('ipsum'))
+
+print('\n\n================== find() =================\n')
+
+
+print('\n\n================== index() =================\n')
+
+print('\t ', line, ' --- >\t', line.index('ipsum'))
+print('\t ', line, ' --- >\t', line.index('ipsum', 7))
+print('\t ', line, ' --- >\t', line.index('ipsum', 24))
+# print('\t ', line, ' --- >\t', line.index('ipsum', 42))
+
+print('\n\n============================================\n')
+
+print('\n\n================== rindex() =================\n')
+
+print('\t ', line, ' --- >\t', line.rindex('ipsum'))
+print('\t ', line, ' --- >\t', line.rindex('ipsum', 0, 40))
+print('\t ', line, ' --- >\t', line.rindex('ipsum', 0, 22))
+# print('\t ', line, ' --- >\t', line.rindex('ipsum', 0, 5))
+
+print('\n\n============================================\n')
+
+
+print('\n\n================== count() =================\n')
+print('\t ', line, ' --- >\t', line.count('ipsum'))
+print('\n\n============================================\n')
+
+
+print('\n\n================== replace() =================\n')
+print('\t ', line, ' --- >\t', line.replace('ipsum', 'yellow'))
+print('\n\n============================================\n')
+
+
+str_1 = '123'
+str_2 = '234'
+str_3 = 'Lorem** 21 ipsum red'
+
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_1, '\t\t\t --- >\t', re.search('12', str_1))
+print('\t ', str_2, '\t\t\t --- >\t', re.search('12', str_2))
+print('\t ', str_3, '\t --- > \t', re.search('12', str_3))
+print('\n\n============================================\n')
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_1, '\t\t\t --- >\t', re.search('[12]', str_1))
+print('\t ', str_2, '\t\t\t --- >\t', re.search('[12]', str_2))
+print('\t ', str_3, '\t --- > \t', re.search('[12]', str_3))
+print('\n\n============================================\n')
+
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_1, '\t\t\t --- >\t', re.search('[0-9]', str_1))
+print('\t ', str_2, '\t\t\t --- >\t', re.search('[0-9]', str_2))
+print('\t ', str_3, '\t --- > \t', re.search('[0-9]', str_3))
+print('\n\n============================================\n')
+
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_1, '\t\t\t --- >\t', re.search('[a-z]', str_1))
+print('\t ', str_2, '\t\t\t --- >\t', re.search('[a-z]', str_2))
+print('\t ', str_3, '\t --- > \t', re.search('[a-z]', str_3))
+print('\n\n============================================\n')
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_1, '\t\t\t --- >\t', re.search('[a-z]', str_1))
+
+match = re.search('[a-z]', str_1)
+if match:
+    print("Find by letter")
+else:
+    print('not found letter')
+
+match = re.search('[a-zA-Z]', str_3)
+print('\t ', str_3, '\t\t\t --- >\t', re.search('[a-zA-Z]', str_3))
+
+if match:
+    print("Find by letter")
+else:
+    print('not found letter')
+print('\n\n============================================\n')
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_1, '\t\t\t --- >\t', re.search('[a-zA-Z]{5}', str_1))
+print('\t ', str_2, '\t\t\t --- >\t', re.search('[a-zA-Z]{5}', str_2))
+print('\t ', str_3, '\t --- > \t', re.search(' [a-zA-Z]{5}', str_3))
+print('\n\n============================================\n')
+
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_1, '\t\t\t --- >\t', re.search('[a-zA-Z]{3}\W', str_1))
+print('\t ', str_2, '\t\t\t --- >\t', re.search('[a-zA-Z]{3}\W', str_2))
+print('\t ', str_3, '\t --- > \t', re.search(' [a-zA-Z]{3}$', str_3))
+print('\n\n============================================\n')
+
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_3, '\t --- > \t', re.search('^\w+', str_3))
+print('\n\n============================================\n')
+
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_3, '\t --- > \t', re.search('[^Lorem]', str_3))
+print('\n\n============================================\n')
+
+
+print('\n\n================== re.search("template",str)  =================\n')
+print('\t ', str_3, '\t --- > \t', re.search('Lorem\**', str_3).group(0))
+print('\n\n============================================\n')
+
+print('\n\n================== re.search("template",str)  =================\n')
+match = re.findall('\w+', str_3)
+print('\t ', str_3, '\t --- > \t', re.findall('\w+', str_3))
+print('\n\n============================================\n')
+for item in match:
+    print(item)
+
+print('\t ', str_3, '\t --- > \t', re.sub('\w{3}$', 'yellow', str_3))
